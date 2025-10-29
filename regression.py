78bed511e7aab74973649beef94d63982d99a09a
B_kumar <96341622+21f1003593@users.noreply.github.com>
2025-10-29 23:27:44 +0530

    Fix regression model implementation in regression.py
    Corrected import statement and fixed model instantiation and variable case errors.

--- regression.py
+++ regression.py
@@ -1,1 +1,15 @@
-from sklearn.linear_model import l
+from sklearn.linear_model import linearRegression
+from sklearn.datasets import load_diabetes
+
+# loading of data
+data = load_diabetes()
+X = data.data
+y = data.target
+
+#train a regression model
+model = LinearRegresssion()
+model.fit(X,y)
+
+#predict on the first 5 samples
+print(model.predict(x[:5]))
+
